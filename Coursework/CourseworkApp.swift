e01ad487758fa375f156a1e80ab981bdd11e3ffa
Jan Lindroos <janlindroos4@gmail.com>
2023-03-19 18:39:35 +0000

    Initial Commit

---
+++ Coursework/CourseworkApp.swift
@@ -0,0 +1,17 @@
+//
+//  CourseworkApp.swift
+//  Coursework
+//
+//  Created by Jan Lindroos on 19/03/2023.
+//
+
+import SwiftUI
+
+@main
+struct CourseworkApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
